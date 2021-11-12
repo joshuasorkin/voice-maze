ed404386d0ab262145cc4657611217c3c6fe9661
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-11-12 13:27:37 -0800

    added Google example speech api page

--- routes/voice.js
+++ routes/voice.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/../static_html/voice.html");
+})
+
+module.exports = router;
